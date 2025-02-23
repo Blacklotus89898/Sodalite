6fd16a3091664f71accdf3c778a66a767712b6b0
Blacklotus88888 <Song.yang.chen89898@gmail.com>
2025-02-23 15:07:40 -0500

    Updates

--- README.md
+++
@@ -1,4 +0,0 @@
-# Sodalite
-This project aims to provide an interactive dashboard for students to collaborate and work together.
-
-Trying to make it native.
--- sodalite-web-front/src/components/container.tsx
+++
@@ -1,8 +0,0 @@
-
-interface ContainerProps {
-    children: React.ReactNode;
-}
-
-export const Container: React.FC<ContainerProps> = ({ children }) => {
-    return <div className="container" style={{ border: '1px solid blue', borderRadius: '15px', padding: "20px" }}>{children}</div>;
-};
